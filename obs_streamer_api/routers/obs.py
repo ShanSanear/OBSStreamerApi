cfbda67aa973351ef4610a814a24204997c870a8
Shan <pruskimateusz93@gmail.com>
2022-02-12 17:42:33 +0100

    Added models to run and stopping of obs

--- obs_streamer_api/routers/obs.py
+++ obs_streamer_api/routers/obs.py
@@ -1,10 +1,13 @@
 import asyncio
-import subprocess
+from datetime import datetime
 from pathlib import Path
+
+import psutil as psutil
+from pydantic import BaseModel
 
 from obs_streamer_api.dependencies import get_obs_ws
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.logger import logger
 
 router = APIRouter(
@@ -16,12 +19,41 @@ OBS_FOLDER = Path(r"D:\obs-studio\bin\64bit")
 OBS_EXE = OBS_FOLDER / "obs64.exe"
 
 
+class OBSState(BaseModel):
+    pid: int
+    name: str
+
+
+class ProcessKilled(BaseModel):
+    pid: int
+    name: str
+    date: datetime
+
+
 @router.post("/run")
-async def run_obs():
+async def run_obs() -> OBSState:
     try:
-        subprocess.Popen([OBS_EXE, '--minimize-to-tray'], cwd=OBS_FOLDER)
+        process = psutil.Popen([OBS_EXE, '--minimize-to-tray'], cwd=OBS_FOLDER)
+        process.name()
+        return OBSState(
+            pid=process.pid,
+            name=process.name()
+        )
     except Exception as exc:
         raise exc
+
+
+@router.post("/stop")
+async def stop_obs(state: OBSState):
+    for proc in psutil.process_iter():
+        if proc.name() == state.name and proc.pid == state.pid:
+            proc.kill()
+            return ProcessKilled(
+                pid=proc.pid,
+                name=proc.name(),
+                date=datetime.utcnow()
+            )
+    raise HTTPException(status_code=404, detail="Process not found")
 
 
 async def run_obs_ws_command(command):
