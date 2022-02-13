fd2c3ddbb36d03a010ff30dbb65e9b24de6c4d74
Shan <pruskimateusz93@gmail.com>
2022-02-13 12:59:20 +0100

    Added handling of input configuration file

---
+++ obs_streamer_api/config.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import dacite
+import toml
+
+
+@dataclass
+class WEBSOCKET:
+    ADDRESS: str
+    PORT: int
+    PASSWORD: str
+
+
+@dataclass
+class OBS:
+    FOLDER: Path
+    EXE: Path
+    WEBSOCKET: WEBSOCKET
+
+
+@dataclass
+class DISCORD:
+    UPDATE_EXE: str
+
+
+@dataclass
+class YOUTUBE:
+    CLIENT_SECRET: str
+    CHANNEL_ID: str
+
+
+@dataclass
+class STORAGE:
+    RECORDING_FOLDER: Path
+
+
+@dataclass
+class Config:
+    OBS: OBS
+    DISCORD: DISCORD
+    YOUTUBE: YOUTUBE
+    STORAGE: STORAGE
+
+
+config = dacite.from_dict(Config, toml.load('config.toml'),
+                          dacite.Config(cast=[Path]))
